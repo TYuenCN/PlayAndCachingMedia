aae9781a403b899bdb6b3ab781eff3ae98e94c51
Yuan Zheng <yuanzheng@sxzw.tv>
2014-02-26 13:40:19 +0800

    Initial Commit

---
+++ PlayAndCachingMedia/YSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YSAppDelegate.h
+//  PlayAndCachingMedia
+//
+//  Created by sxzw on 14-2-26.
+//  Copyright (c) 2014年 YuenSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
